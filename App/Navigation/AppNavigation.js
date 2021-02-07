5c9c382e9ea1add1891df6c27e029af344155019
Jianlong Nie <1317272927@qq.com>
2021-02-07 15:54:40 +0800

    UI:checkoutResume Screen

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -101,7 +101,7 @@ const PrimaryNav = createStackNavigator(
   {
     // Default config for all screens
     headerMode: 'none',
-    initialRouteName: 'InSufficientSalamiCreditScreen',
+    initialRouteName: 'CheckoutResumeScreen',
     navigationOptions: {},
   }
 );

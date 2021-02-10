aa74be110365d52a731d5b98c6b8a640cd152441
Jianlong Nie <1317272927@qq.com>
2021-02-10 17:15:16 +0800

    just merge

--- App/Components/index.js
+++ App/Components/index.js
@@ -1,43 +1,45 @@
-import BottomSheet from './BottomSheet'
-import Button from './Button'
-import Highlighter from './Highlighter'
-import LocationSearchBox from './LocationSearchBox'
-import PasswordInput from './PasswordInput'
-import TextInput from './TextInput'
-import AppBar from './AppBar'
-import Switch from './Switch'
-import Alert from './Alert'
-import RadiusButton from './RadiusButton'
-import MaterialTextInput from './MaterialTextInput'
-import StarRating from './StarRating'
-import Picker from './Picker'
-import QuantitySelector from './QuantitySelector'
-import DescriptionText from './DescriptionText'
-import Progress from './Progress'
-import BottomSheetBackground from './BottomSheetBackground'
-import SegmentedControl from './SegmentedControl'
-import ProductSearchBox from './ProductSearchBox'
-import Selector from './Selector'
+import BottomSheet from './BottomSheet';
+import Button from './Button';
+import Highlighter from './Highlighter';
+import LocationSearchBox from './LocationSearchBox';
+import PasswordInput from './PasswordInput';
+import TextInput from './TextInput';
+import AppBar from './AppBar';
+import Switch from './Switch';
+import Alert from './Alert';
+import RadiusButton from './RadiusButton';
+import MaterialTextInput from './MaterialTextInput';
+import StarRating from './StarRating';
+import Picker from './Picker';
+import QuantitySelector from './QuantitySelector';
+import DescriptionText from './DescriptionText';
+import Progress from './Progress';
+import BottomSheetBackground from './BottomSheetBackground';
+import SegmentedControl from './SegmentedControl';
+import ProductSearchBox from './ProductSearchBox';
+import Selector from './Selector';
+import SearchBox from './SearchBox';
 
 export {
-    BottomSheet,
-    Button,
-    Highlighter,
-    LocationSearchBox,
-    PasswordInput,
-    TextInput,
-    AppBar,
-    Switch,
-    Alert,
-    RadiusButton,
-    MaterialTextInput,
-    StarRating,
-    Picker,
-    QuantitySelector,
-    DescriptionText,
-    Progress,
-    BottomSheetBackground,
-    SegmentedControl,
-    ProductSearchBox,
-    Selector,
-}
+  BottomSheet,
+  Button,
+  Highlighter,
+  LocationSearchBox,
+  PasswordInput,
+  TextInput,
+  AppBar,
+  Switch,
+  Alert,
+  RadiusButton,
+  MaterialTextInput,
+  StarRating,
+  Picker,
+  QuantitySelector,
+  DescriptionText,
+  Progress,
+  BottomSheetBackground,
+  SegmentedControl,
+  ProductSearchBox,
+  Selector,
+  SearchBox,
+};

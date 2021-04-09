e974b5f5303c99ee36151fef0db27344c8423fa6
nigel447 <nigel447@gmail.com>
2021-04-09 11:41:26 +0300

    public apollo client and global cache

---
+++ App/Apollo/public-api.js
@@ -0,0 +1,44 @@
+import { ApolloClient, createHttpLink, gql } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import globalCache from './cache'
+
+/**
+ * need fetch as we are not in a browser
+ */
+import fetch from 'cross-fetch';
+
+/**
+ * base and url path can change for production and development
+ */
+const BASE_GQL_URL = ''
+const URL_PATH_PART = '/somepath/graphql'
+
+
+/**
+ * headers for the public api
+ */
+const pubLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+    }
+});
+
+/**
+ * gql endpoint
+ */
+const httpLink = createHttpLink({
+    uri: `${BASE_GQL_URL}${URL_PATH_PART}`, fetch: fetch,
+});
+
+/**
+ * client for the public api and global cache
+ */
+export const pubClient = new ApolloClient({
+    link: pubLink.concat(httpLink),
+    cache: globalCache,
+})
+

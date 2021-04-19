2c1b98955069fb50ab9abba7558c0cf02fdcbe68
nigel447 <nigel447@gmail.com>
2021-04-19 12:40:30 +0300

    update befor hein merge

--- App/Validation/index.js
+++ App/Validation/index.js
@@ -3,7 +3,31 @@ const EMAIL_REG = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 const PHONE_REG = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
 
+<<<<<<< Updated upstream
 const INTNTL_PHONE_REG =/^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/
+=======
+const INTNTL_PHONE_REG = /^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/
+
+
+/**
+ *  at least 1 lowercase alphabetical character
+ *  at least 1 uppercase alphabetical character
+ *  at least 1 numeric character
+ *  at least one special character
+ *  eight characters or longer
+ */
+const STRONG_PASSWORD_VALIDATION = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+
+/**
+ *  at least 1 lowercase alphabetical character
+ *  at least 1 uppercase alphabetical character
+ *  at least 1 numeric character
+ *  eight characters or longer
+ */
+const WEAK_PASSWORD_VALIDATION = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/
+
+
+>>>>>>> Stashed changes
 
 /**
  * there will be a bunch of validation functions
@@ -21,6 +45,10 @@ export const isValidEmail = (loginInput) => { return EMAIL_REG.test(loginInput) 
 
 export const isValidPhone = (loginInput) => { return INTNTL_PHONE_REG.test(loginInput) }
 
+/** min 8 chars for now */
+export const isValidPassword = (loginInput) => { return WEAK_PASSWORD_VALIDATION.test(loginInput) }
+
+
 /** a function to decide an input is a valid phone or an email */
 export const loginDifferentiator = (loginInput) => {
     let diff = { isValid: true }
@@ -33,4 +61,79 @@ export const loginDifferentiator = (loginInput) => {
         diff.isValid = false
     }
     return diff
+<<<<<<< Updated upstream
+=======
+}
+
+/** a function to decide if a register user request is a valid with correct error flags */
+export const registerValidator = (registerValues) => {
+    console.log(`debug registerValidator ${JSON.stringify(registerValues)}`)
+    let reporter = { validPhoneOrEmail: true, validPassword:true }
+    let { registerInput, psswd } = registerValues
+    // check for missing values
+    let missing = checkMissingRegisterValues(registerValues)
+    if (missing.hasMissing) {
+        // we are missing something
+        reporter.hasMissing = true
+        reporter.missingVal = missing.missingVal
+        return reporter
+    }
+
+    let validPassword = isValidPassword(psswd)
+    if (!validPassword) {
+        reporter.validPassword = false
+        return reporter
+    } 
+
+    let ret = loginDifferentiator(registerInput)
+    if (ret.isValid) {
+        // we have decided lets check email or phone
+        if (ret.isEmail) {
+            reporter.isEmail = true
+        } else {
+            reporter.isPhone = true
+        }
+
+    } else {
+        reporter.validPhoneOrEmail = false
+        return reporter
+    }
+    return reporter
+}
+
+
+
+
+
+
+//================= Private utility functions for validation
+function checkMissingRegisterValues(registerValues) {
+    let { name, lastName, registerInput, psswd } = registerValues
+    let missingVals = { hasMissing: false }
+    // lets check name and last name exist
+    if (typeof name === 'undefined' || name === "") {
+        missingVals.hasMissing = true
+        missingVals.missingVal = 'Name'
+        return missingVals
+
+    }
+    if (typeof lastName === 'undefined' || lastName === "") {
+        missingVals.hasMissing = true
+        missingVals.missingVal = 'Last Name'
+        return missingVals
+    }
+    if (typeof registerInput === 'undefined' || registerInput === "") {
+        missingVals.hasMissing = true
+        missingVals.missingVal = 'Email or Phone'
+        return missingVals
+    }
+
+    if (typeof psswd === 'undefined' || psswd === "") {
+        missingVals.hasMissing = true
+        missingVals.missingVal = 'Password'
+        return missingVals
+    }
+
+    return missingVals
+>>>>>>> Stashed changes
 }

9be4defad82e55666a58020f02bf46e991ec9578
Jianlong Nie <1317272927@qq.com>
2021-04-22 14:48:24 +0800

    edit personal details

--- App/Containers/Orders/RateSeller/index.js
+++ App/Containers/Orders/RateSeller/index.js
@@ -1,155 +1,146 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
-    View,
-    StatusBar,
-    Text,
-    Image,
-    TouchableOpacity,
-    FlatList,
-    ImageBackground,
-} from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import ActionSheet from 'react-native-actionsheet'
-import ImagePicker from 'react-native-image-crop-picker'
+  View,
+  StatusBar,
+  Text,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  ImageBackground,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import ActionSheet from 'react-native-actionsheet';
+import ImagePicker from 'react-native-image-crop-picker';
 
-import styles from './styles'
+import styles from './styles';
 
-import {
-    AppBar,
-    StarRating,
-    TextInput,
-} from '../../../Components'
-import { Colors, Images } from '../../../Themes'
-import NavigationService from '../../../Navigation/NavigationService'
-import { s } from 'react-native-size-matters'
+import { AppBar, StarRating, TextInput } from '../../../Components';
+import { Colors, Images } from '../../../Themes';
+import NavigationService from '../../../Navigation/NavigationService';
+import { s } from 'react-native-size-matters';
 
 class RateSellerScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            images: []
-        }
-    }
+  componentDidMount() {}
 
-    componentDidMount() {
+  onPost = () => {
+    this.props.navigation.state.params.onPost();
+    NavigationService.goBack();
+  };
 
-    }
+  onPressGallery = () => {
+    ImagePicker.openPicker({
+      //cropping: true,
+      includeBase64: true,
+      multiple: true,
+    }).then((image) => {
+      let images = [...image, ...this.state.images];
+      this.setState({ images });
+    });
+  };
 
-    onPost = () => {
-        this.props.navigation.state.params.onPost()
-        NavigationService.goBack()
-    }
+  onPressCamera = () => {
+    ImagePicker.openCamera({
+      cropping: true,
+      includeBase64: true,
+    }).then((image) => {
+      let images = [...image, ...this.state.images];
+      this.setState({ images });
+    });
+  };
 
-    onPressGallery = () => {
-        ImagePicker.openPicker({
-            //cropping: true,
-            includeBase64: true,
-            multiple: true
-        }).then(image => {
-            let images = [...image, ...this.state.images]
-            this.setState({ images })
-        })
-    }
+  showActionSheet = () => {
+    this.ActionSheet.show();
+  };
 
-    onPressCamera = () => {
-        ImagePicker.openCamera({
-            cropping: true,
-            includeBase64: true,
-        }).then(image => {
-            let images = [...image, ...this.state.images]
-            this.setState({ images })
-        })
-    }
+  removeImage = (index) => {
+    let images = [...this.state.images];
+    images.splice(index, 1);
+    this.setState({ images });
+  };
 
-    showActionSheet = () => {
-        this.ActionSheet.show()
-    }
+  renderHeader() {
+    return (
+      <View style={styles.header}>
+        <AppBar
+          title={'Evaluate the seller'}
+          rightButton={() => (
+            <TouchableOpacity onPress={this.onPost}>
+              <Text style={styles.txtSave}>POST</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    );
+  }
 
-    removeImage = (index) => {
-        let images = [...this.state.images]
-        images.splice(index, 1)
-        this.setState({ images })
-    }
+  renderImageItem = ({ item, index }) => {
+    return (
+      <ImageBackground
+        borderRadius={s(10)}
+        source={{ uri: item.path }}
+        style={styles.photoContainer}
+      >
+        <TouchableOpacity
+          onPress={() => this.removeImage(index)}
+          style={styles.btnDeleteContainer}
+        >
+          <Image source={Images.crossMedium} style={styles.icDelete} />
+        </TouchableOpacity>
+      </ImageBackground>
+    );
+  };
 
-    renderHeader() {
-        return (
-            <View style={styles.header}>
-                <AppBar
-                    title={'Evaluate the seller'}
-                    rightButton={() =>
-                        <TouchableOpacity onPress={this.onPost}>
-                            <Text style={styles.txtSave}>POST</Text>
-                        </TouchableOpacity>
-                    }
-                />
-            </View>
-        )
-    }
+  renderBody() {
+    return (
+      <View style={styles.body}>
+        <View style={styles.center}>
+          <Text style={styles.txtProductName}>iPhone 11</Text>
+          <StarRating ratingMode />
+        </View>
 
-    renderImageItem = ({ item, index }) => {
-        return (
-            <ImageBackground
-                borderRadius={s(10)}
-                source={{ uri: item.path }}
-                style={styles.photoContainer}>
-                <TouchableOpacity
-                    onPress={() => this.removeImage(index)}
-                    style={styles.btnDeleteContainer}>
-                    <Image source={Images.crossMedium} style={styles.icDelete} />
-                </TouchableOpacity>
-            </ImageBackground>
-        )
-    }
+        <TextInput
+          style={styles.reviewInput}
+          multiline
+          placeholder={'Write here your review'}
+        />
+      </View>
+    );
+  }
 
-    renderBody() {
-        return (
-            <View style={styles.body}>
-                <View style={styles.center}>
-                    <Text style={styles.txtProductName}>iPhone 11</Text>
-                    <StarRating ratingMode />
-                </View>
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
+          {this.renderHeader()}
 
-                <TextInput
-                    style={styles.reviewInput}
-                    multiline
-                    placeholder={'Write here your review'}
-                />
-            </View>
-        )
-    }
+          {this.renderBody()}
+        </SafeAreaView>
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <StatusBar barStyle='dark-content' />
-                <SafeAreaView
-                    style={styles.container}
-                    edges={['top', 'left', 'right']}>
-
-                    {this.renderHeader()}
-
-                    {this.renderBody()}
-
-                </SafeAreaView>
-
-                <ActionSheet
-                    ref={o => this.ActionSheet = o}
-                    title={'Which one do you like ?'}
-                    options={['Camera', 'Photo Gallery', 'Cancel']}
-                    cancelButtonIndex={2}
-                    //destructiveButtonIndex={1}
-                    onPress={(index) => {
-                        if (index === 0) {
-                            this.onPressCamera()
-                        } else if (index === 1) {
-                            this.onPressGallery()
-                        }
-                    }}
-                />
-            </View>
-        )
-    }
+        <ActionSheet
+          ref={(o) => (this.ActionSheet = o)}
+          title={'Which one do you like ?'}
+          options={['Camera', 'Photo Gallery', 'Cancel']}
+          cancelButtonIndex={2}
+          //destructiveButtonIndex={1}
+          onPress={(index) => {
+            if (index === 0) {
+              this.onPressCamera();
+            } else if (index === 1) {
+              this.onPressGallery();
+            }
+          }}
+        />
+      </View>
+    );
+  }
 }
 
-export default RateSellerScreen
+export default RateSellerScreen;

93bdf9508a9014f089fd385e9e0baea32189498e
Jianlong Nie <1317272927@qq.com>
2021-05-15 21:29:08 +0800

    ds

--- App/Containers/Login/index.js
+++ App/Containers/Login/index.js
@@ -1,228 +1,232 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import {
-    View,
-    StatusBar,
-    Text,
-    TouchableOpacity,
-    Keyboard
-} from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { vs } from 'react-native-size-matters'
-import {useRoute} from '@react-navigation/native'
+  View,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { vs } from 'react-native-size-matters';
+import { useRoute } from '@react-navigation/native';
 
-import {
-    TextInput,
-    Button,
-    PasswordInput,
-    Alert,
-} from '../../Components'
+import { TextInput, Button, PasswordInput, Alert } from '../../Components';
 
-import { Colors } from '../../Themes'
-import styles from './styles'
+import { Colors } from '../../Themes';
+import styles from './styles';
 
 /**
  * validation and jwt modules
  */
-import * as validator from '../../Validation'
-import * as jwt from '../../Apollo/jwt-request'
-import * as storage from '../../Apollo/local-storage'
+import * as validator from '../../Validation';
+import * as jwt from '../../Apollo/jwt-request';
+import * as storage from '../../Apollo/local-storage';
 import { useQuery } from '@apollo/client';
 /** userProfileVar is the variable for the cache to get set  userProfile attributes */
-import { userProfileVar } from '../../Apollo/cache'
-import NavigationService from '../../Navigation/NavigationService'
+import { userProfileVar } from '../../Apollo/cache';
+import NavigationService from '../../Navigation/NavigationService';
 
 function LoginScreen(props) {
-    // refs
-    let passwordInput = null
+  // refs
+  let passwordInput = null;
 
-    let [keyboardHeight, setKeyboardHeight] = useState(0)
-    let [showResetPasswordAlert, setShowResetPasswordAlert] = useState(false)
-    let [showValidationAlert, setShowValidationAlert] = useState(false)
-    let [loginInput, setLoginInput] = useState('')
-    let [psswd, setPsswd] = useState('')
-    const { params } = useRoute();
+  let [keyboardHeight, setKeyboardHeight] = useState(0);
+  let [showResetPasswordAlert, setShowResetPasswordAlert] = useState(false);
+  let [showValidationAlert, setShowValidationAlert] = useState(false);
+  let [loginInput, setLoginInput] = useState('');
+  let [psswd, setPsswd] = useState('');
+  const { params } = useRoute();
 
-    useEffect(() => {
-        Keyboard.addListener('keyboardWillShow', _keyboardWillShow)
-        Keyboard.addListener('keyboardWillHide', _keyboardWillHide)
-        /**
-         * this page is a bit diferent as the navigation params will aways be
-         * undefined unless in the single case where we are coming from
-         * ForgotPassword
-         */
-        if (params === undefined) {
-            //console.log ('debug message caught expected undefined parameter')  
-        } else {
-            //  console.log (`'debug message ${props.navigation.state.params.showEms}`)
-            toggleResetPasswordAlert()
-        }
-
-        return () => {
-            // Anything in here is fired on component unmount.
-            Keyboard.removeListener('keyboardWillShow', _keyboardWillShow)
-            Keyboard.removeListener('keyboardWillHide', _keyboardWillHide)
-        }
-    }, [props]);
-
-    // u can use this to skip login for some development, need to paste an access token to replace somejwt
-    const onDebugSignIn = async () => {
-        userProfileVar({
-            email: loginInput,
-            isAuth: true
-        })
-        storage.setLocalStorageValue(storage.LOCAL_STORAGE_TOKEN_KEY, 'somejwt')
-        NavigationService.navigate('MainScreen')
+  useEffect(() => {
+    Keyboard.addListener('keyboardWillShow', _keyboardWillShow);
+    Keyboard.addListener('keyboardWillHide', _keyboardWillHide);
+    /**
+     * this page is a bit diferent as the navigation params will aways be
+     * undefined unless in the single case where we are coming from
+     * ForgotPassword
+     */
+    if (params === undefined) {
+      //console.log ('debug message caught expected undefined parameter')
+    } else {
+      //  console.log (`'debug message ${props.navigation.state.params.showEms}`)
+      toggleResetPasswordAlert();
     }
 
-    const onSignIn = async() => {
-        // see /home/ubu5/vk-dev/MobileApp/__tests__/v_tests.js  'test determine user input'
-        console.log('onSignIn' + `${loginInput}:::${psswd}`)// to-do remove
-        let ret = validator.loginDifferentiator(loginInput)
-         if (ret.isValid) {
-            // we are good so we can test for email or phone
-             if (ret.isEmail) {
-                let loginRequest = { username: 'billy@silly.com', password: '123456789'}
-                // console.log(profile.data.userProfileVar.email)// to-do remove
-                await jwt.runTokenFlow(loginRequest).then(function (res) {
-                    if (typeof res !== 'undefined') {
-                        console.log(`login ok set auth`)
-                        userProfileVar({
-                            email: loginRequest.username,
-                            isAuth: true
-                        })
-         
-                        let access_token =res.data.access_token
-                        if (access_token === 'undefined') {
-                          console.log('no access token')
-                        } 
-                        storage.setLocalStorageValue(storage.LOCAL_STORAGE_TOKEN_KEY, access_token)
-                        NavigationService.navigate('MainScreen')
-                       }
-                    // need check for status code = 200 
-                    // below is a mock for the expected jwt shpould be something like res.data.<some json token id>
-                     else {
-                        console.log('psswd is not correct')
-                        toggleResetValidationAlert()
-                    }
+    return () => {
+      // Anything in here is fired on component unmount.
+      Keyboard.removeListener('keyboardWillShow', _keyboardWillShow);
+      Keyboard.removeListener('keyboardWillHide', _keyboardWillHide);
+    };
+  }, [props]);
 
-                }).catch(function (err) {
-                    // here we will need to deal with a  status` code 401 and refresh jwt and try again
-                })
-             } else {
-            //     // must be phone   
-                console.log('phone is valid but not implemented')
-                toggleResetValidationAlert()
-            //     userProfileVar({
-            //         phone: loginInput,
-            //         isAuth: true
-            //     })
-             }
-         } else {
-             console.log('data not valid')
-             toggleResetValidationAlert()
-         }
+  // u can use this to skip login for some development, need to paste an access token to replace somejwt
+  const onDebugSignIn = async () => {
+    userProfileVar({
+      email: loginInput,
+      isAuth: true,
+    });
+    storage.setLocalStorageValue(storage.LOCAL_STORAGE_TOKEN_KEY, 'somejwt');
+    NavigationService.navigate('MainScreen');
+  };
+
+  const onSignIn = async () => {
+    // see /home/ubu5/vk-dev/MobileApp/__tests__/v_tests.js  'test determine user input'
+    console.log('onSignIn' + `${loginInput}:::${psswd}`); // to-do remove
+    let ret = validator.loginDifferentiator(loginInput);
+    if (ret.isValid) {
+      // we are good so we can test for email or phone
+      if (ret.isEmail) {
+        let loginRequest = {
+          username: 'billy@silly.com',
+          password: '123456789',
+        };
+        // console.log(profile.data.userProfileVar.email)// to-do remove
+        await jwt
+          .runTokenFlow(loginRequest)
+          .then(function (res) {
+            if (typeof res !== 'undefined') {
+              console.log(`login ok set auth`);
+              userProfileVar({
+                email: loginRequest.username,
+                isAuth: true,
+              });
+
+              let access_token = res.data.access_token;
+              if (access_token === 'undefined') {
+                console.log('no access token');
+              }
+              storage.setLocalStorageValue(
+                storage.LOCAL_STORAGE_TOKEN_KEY,
+                access_token
+              );
+              NavigationService.navigate('MainScreen');
+            }
+            // need check for status code = 200
+            // below is a mock for the expected jwt shpould be something like res.data.<some json token id>
+            else {
+              console.log('psswd is not correct');
+              toggleResetValidationAlert();
+            }
+          })
+          .catch(function (err) {
+            // here we will need to deal with a  status` code 401 and refresh jwt and try again
+          });
+      } else {
+        //     // must be phone
+        console.log('phone is valid but not implemented');
+        toggleResetValidationAlert();
+        //     userProfileVar({
+        //         phone: loginInput,
+        //         isAuth: true
+        //     })
+      }
+    } else {
+      console.log('data not valid');
+      toggleResetValidationAlert();
     }
+  };
 
-    const toggleResetPasswordAlert = () => {
-        setShowResetPasswordAlert(!showResetPasswordAlert)
-    }
+  const toggleResetPasswordAlert = () => {
+    setShowResetPasswordAlert(!showResetPasswordAlert);
+  };
 
-    const toggleResetValidationAlert = () => {
-        setShowValidationAlert(!showValidationAlert)
-    }
+  const toggleResetValidationAlert = () => {
+    setShowValidationAlert(!showValidationAlert);
+  };
 
-    const _keyboardWillShow = (e) => {
-        setKeyboardHeight(e.endCoordinates.height)
-    }
+  const _keyboardWillShow = (e) => {
+    setKeyboardHeight(e.endCoordinates.height);
+  };
 
-    const _keyboardWillHide = () => {
-        setKeyboardHeight(0)
-    }
+  const _keyboardWillHide = () => {
+    setKeyboardHeight(0);
+  };
 
-    const renderResetPasswordAlert = () => {
-        return (
-            <Alert
-                visible={showResetPasswordAlert}
-                title={'Email/SMS Sent'}
-                message={'We have sent you an email, please use the link on it to proceed with your new password creation.'}
-                color={Colors.secondary00}
-                onDismiss={toggleResetPasswordAlert}
-            />
-        )
-    }
-
-    const renderValidationAlert = () => {
-        return (
-            <Alert
-                visible={showValidationAlert}
-                title={'Check Credentials'}
-                message={'message to do'}
-                color={Colors.warning}
-                onDismiss={toggleResetValidationAlert}
-            />
-        )
-    }
-
+  const renderResetPasswordAlert = () => {
     return (
-        <View style={styles.container}>
-            <StatusBar barStyle='dark-content' />
-            <SafeAreaView
-                style={styles.safeArea}
-                edges={['top', 'right', 'left', 'bottom']}
+      <Alert
+        visible={showResetPasswordAlert}
+        title={'Email/SMS Sent'}
+        message={
+          'We have sent you an email, please use the link on it to proceed with your new password creation.'
+        }
+        color={Colors.secondary00}
+        onDismiss={toggleResetPasswordAlert}
+      />
+    );
+  };
+
+  const renderValidationAlert = () => {
+    return (
+      <Alert
+        visible={showValidationAlert}
+        title={'Check Credentials'}
+        message={'message to do'}
+        color={Colors.warning}
+        onDismiss={toggleResetValidationAlert}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView
+        style={styles.safeArea}
+        edges={['top', 'right', 'left', 'bottom']}
+      >
+        <View style={styles.bodyContainer}>
+          <Text style={styles.txt1}>Sign In</Text>
+
+          <Text style={styles.txt2}>
+            Join purchases to get what{'\n'}you want with great discounts
+          </Text>
+
+          <TextInput
+            style={styles.emailInput}
+            placeholder={'Email or phone number'}
+            onSubmitEditing={() => passwordInput.getInnerRef().focus()}
+            returnKeyType={'next'}
+            onChangeText={(text) => setLoginInput(text)}
+          />
+
+          <PasswordInput
+            style={styles.passwordInput}
+            placeholder={'Enter your password'}
+            ref={(r) => (passwordInput = r)}
+            onSubmitEditing={onSignIn}
+            returnKeyType={'done'}
+            onChangeText={(text) => setPsswd(text)}
+          />
+
+          <View style={{ height: keyboardHeight - vs(100) }} />
+
+          <Button
+            onPress={onDebugSignIn}
+            //onPress={onSignIn}
+            text={'SIGN IN'}
+          />
+
+          <View style={styles.row}>
+            <TouchableOpacity
+              onPress={() => NavigationService.navigate('ForgotPasswordScreen')}
             >
-                <View style={styles.bodyContainer}>
+              <Text style={styles.txtAction}>I FORGOT MY PASSWORD</Text>
+            </TouchableOpacity>
 
-                    <Text style={styles.txt1}>Sign In</Text>
-
-                    <Text style={styles.txt2}>
-                        Join purchases to get what{'\n'}you want with great discounts
-                        </Text>
-
-                    <TextInput
-                        style={styles.emailInput}
-                        placeholder={'Email or phone number'}
-                        onSubmitEditing={() => passwordInput.getInnerRef().focus()}
-                        returnKeyType={'next'}
-                        onChangeText={text => setLoginInput(text)}
-                       
-
-                    />
-
-                    <PasswordInput
-                        style={styles.passwordInput}
-                        placeholder={'Enter your password'}
-                        ref={(r) => passwordInput = r}
-                        onSubmitEditing={onSignIn}
-                        returnKeyType={'done'}
-                        onChangeText={text => setPsswd(text)}
-                   
-                    />
-
-                    <View style={{ height: keyboardHeight - vs(100) }} />
-
-                    <Button
-
-                       // onPress={onDebugSignIn}
-                        onPress={onSignIn}
-                        text={'SIGN IN'}
-                    />
-
-                    <View style={styles.row}>
-                        <TouchableOpacity onPress={() => NavigationService.navigate('ForgotPasswordScreen')}>
-                            <Text style={styles.txtAction}>I FORGOT MY PASSWORD</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={() => NavigationService.navigate('RegisterScreen')}>
-                            <Text style={styles.txtAction}>REGISTER</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </SafeAreaView>
-
-            {renderResetPasswordAlert()}
-            {renderValidationAlert()}
+            <TouchableOpacity
+              onPress={() => NavigationService.navigate('RegisterScreen')}
+            >
+              <Text style={styles.txtAction}>REGISTER</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-    )
+      </SafeAreaView>
+
+      {renderResetPasswordAlert()}
+      {renderValidationAlert()}
+    </View>
+  );
 }
 
 export default LoginScreen;

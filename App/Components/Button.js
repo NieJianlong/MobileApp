01a05fb5671d4108fcd9b86465c019def79c293f
Jianlong Nie <1317272927@qq.com>
2021-05-17 17:26:35 +0800

    fix android bugs

--- App/Components/Button.js
+++ App/Components/Button.js
@@ -1,77 +1,79 @@
-import React, { Component } from 'react'
-import {
-    Text,
-    View,
-    Image,
-    TouchableOpacity
-} from 'react-native'
-import { ScaledSheet } from 'react-native-size-matters'
-import { Fonts, Colors } from '../Themes'
-import PropTypes from 'prop-types'
-import AppConfig from '../Config/AppConfig'
+import React, { Component } from 'react';
+import { Text, View, Image } from 'react-native';
+import { ScaledSheet } from 'react-native-size-matters';
+import { Fonts, Colors } from '../Themes';
+import PropTypes from 'prop-types';
+import AppConfig from '../Config/AppConfig';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 class Button extends Component {
-    render() {
-        const {
-            text,
-            onPress,
-            disabled,
-            backgroundColor,
-            textColor,
-            disabledColor,
-            prefixIcon
-        } = this.props
+  render() {
+    const {
+      text,
+      onPress,
+      disabled,
+      backgroundColor,
+      textColor,
+      disabledColor,
+      prefixIcon,
+    } = this.props;
 
-        return (
-            <TouchableOpacity
-                disabled={disabled}
-                style={[styles.container, { backgroundColor: backgroundColor }, disabled && { opacity: 0.25 }]}
-                onPress={onPress}>
-                {
-                    prefixIcon &&
-                    <Image style={[styles.icon, { tintColor: textColor }]} source={prefixIcon} />
-                }
-                <Text
-                    style={[styles.txt, { color: textColor ?? Colors.white }]}
-                >{text}</Text>
-            </TouchableOpacity>
-        )
-    }
+    return (
+      <TouchableOpacity
+        disabled={disabled}
+        style={[
+          styles.container,
+          { backgroundColor: backgroundColor },
+          disabled && { opacity: 0.25 },
+        ]}
+        onPress={onPress}
+      >
+        {prefixIcon && (
+          <Image
+            style={[styles.icon, { tintColor: textColor }]}
+            source={prefixIcon}
+          />
+        )}
+        <Text style={[styles.txt, { color: textColor ?? Colors.white }]}>
+          {text}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
 }
-
 
 Button.propTypes = {
-    text: PropTypes.string,
-    onPress: PropTypes.any,
-    disabled: PropTypes.bool,
-    backgroundColor: PropTypes.string,
-    textColor: PropTypes.string,
-    disabledColor: PropTypes.string,
-}
+  text: PropTypes.string,
+  onPress: PropTypes.any,
+  disabled: PropTypes.bool,
+  backgroundColor: PropTypes.string,
+  textColor: PropTypes.string,
+  disabledColor: PropTypes.string,
+};
 
 Button.defaultProps = {
-    backgroundColor: Colors.primary,
-    textColor: Colors.white,
-}
+  backgroundColor: Colors.primary,
+  textColor: Colors.white,
+};
 
 const styles = ScaledSheet.create({
-    container: {
-        height: '44@vs',
-        backgroundColor: '#7FFFD4',
-        borderRadius: '22@vs',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row'
-    },
-    txt: {
-        color: 'rgb(24,24,101)',
-        fontSize: AppConfig.fontSize,
-        fontFamily: Fonts.semibold,
-    },
-    icon: {
-        width: '20@s',
-        height: '20@s',
-        marginRight: '5@s'
-    }
-})
-export default Button
+  container: {
+    height: '44@vs',
+    backgroundColor: '#7FFFD4',
+    borderRadius: '22@vs',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  txt: {
+    color: 'rgb(24,24,101)',
+    fontSize: AppConfig.fontSize,
+    fontFamily: Fonts.semibold,
+  },
+  icon: {
+    width: '20@s',
+    height: '20@s',
+    marginRight: '5@s',
+  },
+});
+export default Button;

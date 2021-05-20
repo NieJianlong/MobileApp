1c3b785646a8145cc23240657da22528d93fa702
Jianlong Nie <1317272927@qq.com>
2021-05-20 11:29:52 +0800

    auto login when app open

--- App/Containers/Launch/index.js
+++ App/Containers/Launch/index.js
@@ -1,39 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component, useCallback, useEffect } from 'react';
+import { View, StatusBar, Image } from 'react-native';
+import { runTokenFlow } from '../../Apollo/jwt-request';
 import {
-    View,
-    StatusBar,
-    Image
-} from 'react-native'
-import NavigationService from '../../Navigation/NavigationService'
+  getLocalStorageValue,
+  LOCAL_STORAGE_TOKEN_KEY,
+  LOCAL_STORAGE_USER_NAME,
+  LOCAL_STORAGE_USER_PASSWORD,
+  setLocalStorageValue,
+} from '../../Apollo/local-storage';
+import NavigationService from '../../Navigation/NavigationService';
 
-import { Images } from '../../Themes'
-import styles from './styles'
+import { Images } from '../../Themes';
+import styles from './styles';
 
-class LaunchScreen extends Component {
-
-    componentDidMount() {
-        setTimeout(() => {
-            //this.props.navigation.navigate('OnboardingScreen')
-            NavigationService.navigate('OnboardingScreen')
-        }, 2000)
+export default function LaunchScreen() {
+  const autoSignIn = useCallback(async () => {
+    //get username and possword from localStorage
+    const username = await getLocalStorageValue(LOCAL_STORAGE_USER_NAME);
+    const password = await getLocalStorageValue(LOCAL_STORAGE_USER_PASSWORD);
+    //if username && password exits,we can login auto
+    if (username && password) {
+      const { data } = await runTokenFlow({ username, password });
+      let access_token = data.access_token;
+      if (access_token === 'undefined') {
+        console.log('no access token');
+      }
+      setLocalStorageValue(LOCAL_STORAGE_TOKEN_KEY, access_token);
+      NavigationService.navigate('MainScreen');
+    } else {
+      setTimeout(() => {
+        //this.props.navigation.navigate('OnboardingScreen')
+        NavigationService.navigate('OnboardingScreen');
+      }, 2000);
     }
+  }, []);
+  //when app open,when can do auto login
+  useEffect(() => {
+    autoSignIn();
+  }, [autoSignIn]);
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <StatusBar
-                    barStyle='light-content'
-                    translucent
-                    backgroundColor={'rgba(0,0,0,0.0)'}
-                />
-                <Image
-                    source={Images.logo2}
-                    style={styles.logo}
-                    resizeMode={'contain'}
-                />
-            </View>
-        )
-    }
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor={'rgba(0,0,0,0.0)'}
+      />
+      <Image source={Images.logo2} style={styles.logo} resizeMode={'contain'} />
+    </View>
+  );
 }
-
-export default LaunchScreen

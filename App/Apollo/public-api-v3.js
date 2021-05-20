36d637be9ec9a187d8fefa46f94efac07c576463
Jianlong Nie <1317272927@qq.com>
2021-05-20 12:01:46 +0800

    sd

--- App/Apollo/public-api-v3.js
+++ App/Apollo/public-api-v3.js
@@ -1,67 +1,70 @@
-import { ApolloClient, createHttpLink, ApolloLink, concat } from '@apollo/client';
-import globalCache from './cache'
+import {
+  ApolloClient,
+  createHttpLink,
+  ApolloLink,
+  concat,
+} from '@apollo/client';
+import globalCache from './cache';
 
 /**
  * need fetch as we are not in a browser
  */
- import fetch from 'cross-fetch';
+import fetch from 'cross-fetch';
 
-
- /**
-  * 
-  * @param {*} endpoint 
-  * @returns 
-  */
- export const PUBLIC_CLIENT_ENDPOINT = 'http://ec2-18-191-146-179.us-east-2.compute.amazonaws.com:8082/graphql'
+/**
+ *
+ * @param {*} endpoint
+ * @returns
+ */
+export const PUBLIC_CLIENT_ENDPOINT =
+  'http://ec2-18-191-146-179.us-east-2.compute.amazonaws.com:8082/graphql';
 
 export const endPointClient = async (endpoint) => {
-    const authMiddleware = new ApolloLink((operation, forward) => {
-
-        // add headers
-        operation.setContext({
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            }
-        });
-
-        return forward(operation);
+  const authMiddleware = new ApolloLink((operation, forward) => {
+    // add headers
+    operation.setContext({
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
     });
 
-    const httpLink = createHttpLink({
-        uri: endpoint, fetch: fetch,
+    return forward(operation);
+  });
+
+  const httpLink = createHttpLink({
+    uri: endpoint,
+    fetch: fetch,
+  });
+
+  return new ApolloClient({
+    cache: globalCache,
+    link: concat(authMiddleware, httpLink),
+  });
+};
+
+export const pubClient = async () => {
+  let endpoint =
+    'http://ec2-18-191-146-179.us-east-2.compute.amazonaws.com:8082/graphql';
+  const authMiddleware = new ApolloLink((operation, forward) => {
+    // add headers
+    operation.setContext({
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
     });
 
-    return new ApolloClient({
-        cache: globalCache,
-        link: concat(authMiddleware, httpLink),
-    });
-}
+    return forward(operation);
+  });
 
+  const httpLink = createHttpLink({
+    uri: endpoint,
+    fetch: fetch,
+  });
 
-
-export const pubClient = async ( ) => {
-    let endpoint = 'http://ec2-18-191-146-179.us-east-2.compute.amazonaws.com:8082/graphql'
-    const authMiddleware = new ApolloLink((operation, forward) => {
-
-        // add headers
-        operation.setContext({
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            }
-        });
-
-        return forward(operation);
-    });
-
-    const httpLink = createHttpLink({
-        uri: endpoint, fetch: fetch,
-    });
-
-    return new ApolloClient({
-        cache: globalCache,
-        link: concat(authMiddleware, httpLink),
-    });
-}
- 
+  return new ApolloClient({
+    cache: globalCache,
+    link: concat(authMiddleware, httpLink),
+  });
+};

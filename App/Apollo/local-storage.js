161131ffa8967976a86e6220cbe1b6185843da0f
Jianlong Nie <1317272927@qq.com>
2021-05-20 11:29:52 +0800

    auto login when app open

--- App/Apollo/local-storage.js
+++ App/Apollo/local-storage.js
@@ -4,6 +4,9 @@ export const LOCAL_STORAGE_TOKEN_KEY = '@local_storage_token_key'
 export const GUEST_BUYER_ID_KEY = '@local_storage_guest_id_key'
 export const REGISTER_BUYER_ID_KEY = '@local_storage_register_id_key'
 
+export const LOCAL_STORAGE_USER_NAME = '@local_storage_user_name'
+export const LOCAL_STORAGE_USER_PASSWORD = '@local_storage_user_password'
+
 export const setLocalStorageValue = async (key, val) => {
  // console.log(`setLocalStorageValue key:${key} val ${val}`)
  

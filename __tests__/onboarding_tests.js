c211cb977e47bf18c39b74a56d163f64f576a5af
Jianlong Nie <1317272927@qq.com>
2021-05-21 15:27:25 +0800

    updates

--- __tests__/onboarding_tests.js
+++ __tests__/onboarding_tests.js
@@ -1,4 +1,4 @@
-import { endPointClient } from "../App/Apollo/public-api-v3";
+import { endPointClient } from '../App/Apollo/public-api-v3';
 
 import {
   CREATE_ADDRESS,
@@ -7,27 +7,27 @@ import {
   REGISTER_BUYER,
   CREATE_GUEST_BUYER,
   NEW_ID,
-} from "../App/Apollo/mutations/mutations_user";
+} from '../App/Apollo/mutations/mutations_user';
 import {
   ADDRESSES,
   FIND_BUYER_ADDRESS_BY_ID,
   FIND_BUYER_DEFAULT_ADDRESS_BY_ID,
   FIND_GUEST_BUYER_ADDRESS_BY_ID,
   FIND_GUEST_BUYER_DEFAULT_ADDRESS_BY_ID,
-} from "../App/Apollo/queries/queries_user";
+} from '../App/Apollo/queries/queries_user';
 
 /**
  * rest and gql api call tests
  */
-import * as jwt from "../App/Apollo/jwt-request";
+import * as jwt from '../App/Apollo/jwt-request';
 
-import * as storage from "../App/Apollo/local-storage";
+import * as storage from '../App/Apollo/local-storage';
 
 let url =
-  "http://ec2-18-191-146-179.us-east-2.compute.amazonaws.com:8082/graphql";
+  'http://ec2-18-191-146-179.us-east-2.compute.amazonaws.com:8082/graphql';
 
-jest.mock("@react-native-community/async-storage", () =>
-  require("@react-native-community/async-storage/jest/async-storage-mock")
+jest.mock('@react-native-community/async-storage', () =>
+  require('@react-native-community/async-storage/jest/async-storage-mock')
 );
 
 /** tests for the onboarding endpoints */
@@ -42,21 +42,21 @@ jest.mock("@react-native-community/async-storage", () =>
 // update numbers so no xxx exists errors
 // node_modules/jest/bin/jest.js -t "test guest buyer"
 //    {"data":{"createGuestBuyer":{"buyerId":"204750d9-62cc-419d-b8e3-ac0b285c18dd","__typename":"BuyerProfileResponse"}}}
-it("test guest buyer", async () => {
+it('test guest buyer', async () => {
   let BuyerProfileRequestForCreate = {
-    userName: "buyer3",
-    firstName: "buyer33",
-    lastName: "buyer333",
-    geoLocation: "1.2.3.4",
-    phoneNumber: "+3331171230012",
+    userName: 'buyer3',
+    firstName: 'buyer33',
+    lastName: 'buyer333',
+    geoLocation: '1.2.3.4',
+    phoneNumber: '+3331171230012',
     guestBuyer: true,
-    email: "g33buyer11@email.com",
-    userType: "BUYER",
-    password: "333#$Tymkop22",
+    email: 'g33buyer11@email.com',
+    userType: 'BUYER',
+    password: '333#$Tymkop22',
     oneClickPurchaseOn: true,
-    areaRegion: "",
-    languages: ["EN"],
-    currencies: ["EUR"],
+    areaRegion: '',
+    languages: ['EN'],
+    currencies: ['EUR'],
   };
   let client = await endPointClient(url);
   let ret = await client
@@ -66,18 +66,18 @@ it("test guest buyer", async () => {
     })
     .then((result) => result)
     .catch((err) => {
-      console.log("mutation error " + err);
+      console.log('mutation error ' + err);
       return;
     });
 
-  if (typeof ret !== "undefined") {
+  if (typeof ret !== 'undefined') {
     console.log(JSON.stringify(ret));
   }
 });
 
 /** test with minimal input */
 // node_modules/jest/bin/jest.js -t "test guest buyer 2"
-it("test guest buyer 2", async () => {
+it('test guest buyer 2', async () => {
   let BuyerProfileRequestForCreate = {
     guestBuyer: true,
   };
@@ -89,11 +89,11 @@ it("test guest buyer 2", async () => {
     })
     .then((result) => result)
     .catch((err) => {
-      console.log("mutation error " + err);
+      console.log('mutation error ' + err);
       return;
     });
 
-  if (typeof ret !== "undefined") {
+  if (typeof ret !== 'undefined') {
     console.log(JSON.stringify(ret));
   }
 });
@@ -102,21 +102,21 @@ it("test guest buyer 2", async () => {
 // update numbers so no xxx exists errors
 // node_modules/jest/bin/jest.js -t "test register buyer"
 //  {"data":{"registerBuyer":{"buyerId":"c4f06cac-3661-408f-9d04-af3f7090c6db","__typename":"BuyerProfileResponse"}}}
-it("test register buyer", async () => {
+it('test register buyer', async () => {
   let BuyerProfileRequestForCreate = {
-    userName: "cu3634",
-    firstName: "cu363Y4",
-    lastName: "cu336YR",
-    geoLocation: "1.2.3.44",
-    phoneNumber: "+33267700124",
+    userName: 'cu3634',
+    firstName: 'cu363Y4',
+    lastName: 'cu336YR',
+    geoLocation: '1.2.3.44',
+    phoneNumber: '+33267700124',
     guestBuyer: false,
-    email: "cu@email.com",
-    userType: "BUYER",
-    password: "Wwwwwww8",
+    email: 'cu@email.com',
+    userType: 'BUYER',
+    password: 'Wwwwwww8',
     oneClickPurchaseOn: true,
-    areaRegion: "",
-    languages: ["EN"],
-    currencies: ["EUR"],
+    areaRegion: '',
+    languages: ['EN'],
+    currencies: ['EUR'],
   };
   let client = await endPointClient(url);
   let ret = await client
@@ -126,45 +126,45 @@ it("test register buyer", async () => {
     })
     .then((result) => result)
     .catch((err) => {
-      console.log("mutation error " + err);
+      console.log('mutation error ' + err);
       return;
     });
 
-  if (typeof ret !== "undefined") {
+  if (typeof ret !== 'undefined') {
     console.log(JSON.stringify(ret));
   }
 });
 
 // node_modules/jest/bin/jest.js -t "test create address"
-it("test create address", async () => {
+it('test create address', async () => {
   // {"data":{"createAddress":{"addressId":"b105be7f-693d-41bb-a727-b301e813096a","__typename":"AddressResponse"}}}
   let AddressRequestForCreate = {
     defaultAddress: true,
-    addressType: "SHIPPING",
-    flat: "flat",
-    floor: "floor",
-    district: "13",
-    streetAddress1: "street_1",
-    streetAddress2: "street_2",
-    streetAddress3: "street_3",
-    block: "block",
-    building: "building",
-    houseNumber: "11",
-    villageArea: "village_area",
-    provinceState: "state",
-    landMark: "",
-    pinCode: "1138",
-    country: "UK",
-    areaCode: "XYZ123",
-    referenceId: "98ecb643-a64c-469d-99c7-fd559d244019",
+    addressType: 'SHIPPING',
+    flat: 'flat',
+    floor: 'floor',
+    district: '13',
+    streetAddress1: 'street_1',
+    streetAddress2: 'street_2',
+    streetAddress3: 'street_3',
+    block: 'block',
+    building: 'building',
+    houseNumber: '11',
+    villageArea: 'village_area',
+    provinceState: 'state',
+    landMark: '',
+    pinCode: '1138',
+    country: 'UK',
+    areaCode: 'XYZ123',
+    referenceId: '98ecb643-a64c-469d-99c7-fd559d244019',
   };
 
   let AddressRequestForCreate2 = {
     defaultAddress: true,
-    addressType: "SHIPPING",
-    provinceState: "state",
-    pinCode: "1138",
-    referenceId: "98ecb643-a64c-469d-99c7-fd559d244019",
+    addressType: 'SHIPPING',
+    provinceState: 'state',
+    pinCode: '1138',
+    referenceId: '98ecb643-a64c-469d-99c7-fd559d244019',
   };
   let client = await endPointClient(url);
   let ret = await client
@@ -174,51 +174,51 @@ it("test create address", async () => {
     })
     .then((result) => result)
     .catch((err) => {
-      console.log("mutation error " + err);
+      console.log('mutation error ' + err);
       return;
     });
 
-  if (typeof ret !== "undefined") {
+  if (typeof ret !== 'undefined') {
     console.log(JSON.stringify(ret));
   }
 });
 
 // node_modules/jest/bin/jest.js -t "test getGuestBuyerAddressesById"
-it("test getGuestBuyerAddressesById", async () => {
+it('test getGuestBuyerAddressesById', async () => {
   // public api
   let client = await endPointClient(url);
   let ret = await client
     .query({
       query: FIND_GUEST_BUYER_ADDRESS_BY_ID,
-      variables: { buyerId: "99264247-c58c-4a77-bb61-559b6226db2c" },
+      variables: { buyerId: '99264247-c58c-4a77-bb61-559b6226db2c' },
     })
     .then((result) => result)
     .catch((err) => {
-      console.log("query error " + err);
+      console.log('query error ' + err);
       return;
     });
 
-  if (typeof ret !== "undefined") {
+  if (typeof ret !== 'undefined') {
     console.log(JSON.stringify(ret));
   }
 });
 
 // node_modules/jest/bin/jest.js -t "test getGuestBuyerDefaultAddressByBuyerId"
-it("test getGuestBuyerDefaultAddressByBuyerId", async () => {
+it('test getGuestBuyerDefaultAddressByBuyerId', async () => {
   //     referenceId: "98ecb643-a64c-469d-99c7-fd559d244019"
   let client = await endPointClient(url);
   let ret = await client
     .query({
       query: FIND_GUEST_BUYER_DEFAULT_ADDRESS_BY_ID,
-      variables: { buyerId: "a89b217a-7d51-4a31-9158-3a1e27857f9a" },
+      variables: { buyerId: 'a89b217a-7d51-4a31-9158-3a1e27857f9a' },
     })
     .then((result) => result)
     .catch((err) => {
-      console.log("query error " + err);
+      console.log('query error ' + err);
       return;
     });
 
-  if (typeof ret !== "undefined") {
+  if (typeof ret !== 'undefined') {
     console.log(JSON.stringify(ret));
   }
 });

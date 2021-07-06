9a9541014e3d9b2f75f85b43d468d06db111a91a
Jianlong Nie <1317272927@qq.com>
2021-07-06 17:50:06 +0800

    updates payments list

--- App/Containers/OneClickPurchase/styles.js
+++ App/Containers/OneClickPurchase/styles.js
@@ -1,69 +1,67 @@
-import { ScaledSheet } from 'react-native-size-matters';
-import { ApplicationStyles, Colors, Fonts } from '../../Themes';
-import { isIphoneX } from 'react-native-iphone-x-helper';
-import AppConfig from '../../Config/AppConfig';
+import { ScaledSheet } from "react-native-size-matters";
+import { ApplicationStyles, Colors, Fonts } from "../../Themes";
+import { isIphoneX } from "react-native-iphone-x-helper";
+import AppConfig from "../../Config/AppConfig";
 
 export default ScaledSheet.create({
   ...ApplicationStyles.screen,
   container: {
     flex: 1,
     backgroundColor: Colors.background,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-  
-
   safeArea: {
     flex: 1,
-    width: '100%',
+    width: "100%",
   },
   bodyContainer: {
-    paddingHorizontal: '15@s',
+    paddingHorizontal: "15@s",
     paddingBottom: 170,
   },
   item: {
-    marginTop: '15@vs',
-    backgroundColor: 'white',
-    borderRadius: '16@s',
-    height: '122@vs',
+    marginTop: "15@vs",
+    backgroundColor: "white",
+    borderRadius: "16@s",
+    height: "122@vs",
     paddingHorizontal: AppConfig.paddingHorizontal,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   otpContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '20@vs',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "20@vs",
   },
   txtInput: {
-    width: '40@s',
-    height: '48@vs',
+    width: "40@s",
+    height: "48@vs",
     backgroundColor: Colors.white,
-    borderRadius: '40@s',
+    borderRadius: "40@s",
     borderWidth: 1,
     borderColor: Colors.grey20,
-    marginHorizontal: '16@s',
-    textAlign: 'center',
+    marginHorizontal: "16@s",
+    textAlign: "center",
     fontFamily: Fonts.semibold,
-    fontSize: '20@s',
+    fontSize: "20@s",
     color: Colors.black,
   },
   txtInputFocused: {
-    width: '40@s',
-    height: '48@vs',
+    width: "40@s",
+    height: "48@vs",
     backgroundColor: Colors.white,
-    borderRadius: '40@s',
+    borderRadius: "40@s",
     borderWidth: 1,
     borderColor: Colors.black,
-    marginHorizontal: '16@s',
-    textAlign: 'center',
+    marginHorizontal: "16@s",
+    textAlign: "center",
     fontFamily: Fonts.semibold,
-    fontSize: '20@s',
+    fontSize: "20@s",
     color: Colors.black,
   },
   btnResendCode: {
-    paddingTop: '20@vs',
-    alignSelf: 'center',
-    marginBottom: '10@vs',
+    paddingTop: "20@vs",
+    alignSelf: "center",
+    marginBottom: "10@vs",
   },
 });
--- App/Containers/UserInfo/InfoList/Payments.js
+++ App/Containers/UserInfo/InfoList/Payments.js
@@ -1,11 +1,8 @@
-import React, { useCallback, useEffect } from "react";
-import { View, FlatList } from "react-native";
+import React from "react";
+import { FlatList } from "react-native";
 import TextTip from "../../../Components/EmptyReminder";
 import NavigationService from "../../../Navigation/NavigationService";
 import PaymentItem from "./PaymentItem";
-import { useQuery } from "@apollo/client";
-import { PAYMENT_METHODS_BY_ID } from "../../../Apollo/queries/queries_user";
-import { useFocusEffect } from "@react-navigation/native";
 /**
  * @description:Display my address, list of my payment methods, display bill detail
  * @param {*} item Menu Item with a special configuration
@@ -14,36 +11,10 @@ import { useFocusEffect } from "@react-navigation/native";
  * @param {*} showSheet Display the Acction Sheet for the home page
  * @return {*}
  */
-export default function Payments({ dispatch, xIndex }) {
-  const { refetch, data } = useQuery(PAYMENT_METHODS_BY_ID, {
-    variables: { buyerId: global.buyerId },
-    context: {
-      headers: {
-        isPrivate: true,
-      },
-    },
-    onCompleted: (res) => {},
-    onError: (res) => {},
-  });
-
-  const refreshData = useCallback(() => {
-    refetch();
-  }, [refetch]);
-  useFocusEffect(refreshData);
-  useEffect(() => {
-    dispatch({
-      type: "rightButtonShow",
-      payload: false,
-    });
-  }, [dispatch]);
-  useEffect(() => {
-    if (xIndex === 0) {
-      refreshData();
-    }
-  }, [refreshData, xIndex]);
+export default function Payments({ data, refetch }) {
   return (
     <FlatList
-      data={data?.paymentDetailsByBuyerId}
+      data={data}
       ListEmptyComponent={
         <TextTip
           textTip="You haven't added any payment  method yet"

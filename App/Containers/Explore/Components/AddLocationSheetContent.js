362fdba131ee84b97121ed83aed45e67734dd9c9
Jianlong Nie <1317272927@qq.com>
2021-07-24 14:43:43 +0800

    add guest address

--- App/Containers/Explore/Components/AddLocationSheetContent.js
+++ App/Containers/Explore/Components/AddLocationSheetContent.js
@@ -1,238 +1,258 @@
-import React, {
-  useRef,
-  useContext,
-  useCallback,
-  useState,
-  useEffect,
-} from "react";
-import {
-  View,
-  Text,
-  Platform,
-  TouchableOpacity as RNTouchableOpacity,
-} from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { View, StatusBar, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { vs } from "react-native-size-matters";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { TextInput } from "../../../Components";
-import styles from "../styles";
+import {
+  AppBar,
+  Switch,
+  RightButton,
+  MaterialTextInput,
+  Selector,
+} from "../../../Components";
+import styles from "./styles";
+import NavigationService from "../../../Navigation/NavigationService";
+import colors from "../../../Themes/Colors";
+import { useRoute } from "@react-navigation/native";
+import { useMutation } from "@apollo/client";
+import {
+  CREATE_ADDRESS,
+  UPDATE_ADDRESS,
+} from "../../../Apollo/mutations/mutations_user";
 import { AlertContext } from "../../Root/GlobalContext";
-import * as aQM from "../gql/explore_queries";
-import * as gqlMappers from "../gql/gql_mappers";
-import { userProfileVar, localCartVar } from "../../../Apollo/cache";
-import { TouchableOpacity as GHTouchableOpacity } from "react-native-gesture-handler";
-import { useMutation, useReactiveVar } from "@apollo/client";
-import AsyncStorage from "@react-native-community/async-storage";
 
-import { client } from "../../../Apollo/apolloClient";
-import { GUEST_BUYER_ID_KEY } from "../../../Apollo/local-storage";
-
-const TouchableOpacity =
-  Platform.OS === "ios" ? RNTouchableOpacity : GHTouchableOpacity;
-/**
- * queries for address
- */
-export default function AddLocationSheet() {
-  let stateInput = useRef();
-  let cityInput = useRef();
-  let villageInput = useRef();
-  let houseNumberInput = useRef();
-  let flatNumberInput = useRef();
-  let landmarkInput = useRef();
+function AddLocationSheetContent(props) {
   const { dispatch } = useContext(AlertContext);
-
-  // mayby we can simplity this
-  const [pinCodeV, setPinCode] = useState(0);
-  const [stateV, setStateV] = useState("");
-  const [cityV, setCityV] = useState("");
-  const [areaV, setAreaV] = useState("");
-  const [numHV, setNumHV] = useState("");
-  const [numFV, setNumFV] = useState("");
-  const [markN, setMarkV] = useState("");
-  const [buyerId, setBuyerId] = useState("");
-
-  const userProfileVarReactive = useReactiveVar(userProfileVar);
-
+  const [name, setName] = useState("");
+  const [streetName, setStreetName] = useState("");
+  const [streetNum, setStreetNum] = useState("");
+  const [door, setDoor] = useState("");
+  const [city, setCity] = useState("");
+  const [mstate, setMstate] = useState("");
+  const [pincode, setPincode] = useState("");
+  const [country, setCountry] = useState("");
+  const [disable, setDisable] = useState(true);
+  const [landMark, setLandMark] = useState("");
+  const [asDefault, setAsDefault] = useState(false);
   useEffect(() => {
-    getBuyerId();
-  });
-
-  const getBuyerId = async () => {
-    let isAuth = userProfileVarReactive.isAuth;
-    if (isAuth) {
-      let buyId = await AsyncStorage.getItem(userProfileVar().email);
-      setBuyerId(buyId);
+    if (
+      name.length === 0 ||
+      streetName.length === 0 ||
+      streetNum.length === 0 ||
+      door.length === 0 ||
+      city.length === 0 ||
+      mstate.length === 0 ||
+      pincode.length === 0 ||
+      country.length === 0 ||
+      landMark.length === 0
+    ) {
+      setDisable(true);
     } else {
-      // this is the guest situation and the device id maps to buyer id
-      // should have been set in onboarding
-      let buyId = await AsyncStorage.getItem(GUEST_BUYER_ID_KEY);
-      setBuyerId(buyId);
+      setDisable(false);
     }
-  };
-
-  const toggleAddLocationSheet = useCallback(() => {
-    // found issues with updating state here
-    dispatch({
-      type: "changSheetState",
-      payload: {
-        showSheet: false,
-        height: 600,
-        children: () => null,
-        sheetTitle: "",
-      },
-    });
-  }, [dispatch]);
-
+  }, [
+    name,
+    streetName,
+    streetNum,
+    door,
+    city,
+    mstate,
+    pincode,
+    country,
+    landMark,
+  ]);
   let AddressRequestForCreate = {
-    pinCode: pinCodeV,
-    defaultAddress: true,
+    pinCode: pincode,
+    defaultAddress: asDefault,
     addressType: "SHIPPING",
-    provinceState: stateV,
-    townCity: cityV,
-    flat: numFV,
-    villageArea: areaV,
-    houseNumber: numHV,
-    landMark: markN,
-    referenceId: buyerId,
+    provinceState: mstate,
+    townCity: city,
+    flat: door,
+    villageArea: streetName,
+    houseNumber: streetNum,
+    landMark: landMark,
+    streetAddress1: name,
+    country,
+    referenceId: global.buyerId,
   };
-  /**
-   * CREATE_ADDRESS is public api
-   * run the CREATE_ADDRESS mutation
-   * see './gql/explore_queries'
-   *
-   * auth flow use buyer id local storage  email key
-   * guest flow use guest buyer id in local storage device id key
-   */
-  const [runAddAddessMutation, { data }] = useMutation(aQM.CREATE_ADDRESS, {
-    context: {
-      headers: { isPrivate: false },
-      variables: { request: AddressRequestForCreate },
-      onCompleted: (data) => createAddressOnComplete(data),
-      onError: (error) =>
-        console.error("AddLocationSheet runAddAddessMutation Error ", error),
+  const [addAddress] = useMutation(CREATE_ADDRESS, {
+    variables: {
+      request: AddressRequestForCreate,
+    },
+    onCompleted: (res) => {
+      dispatch({
+        type: "changAlertState",
+        payload: {
+          visible: true,
+          message: "New address added.",
+          color: colors.success,
+          title: "Address Added",
+        },
+      });
+      dispatch({ type: "hideloading" });
+      NavigationService.goBack();
+    },
+    onError: (error) => {
+      dispatch({ type: "hideloading" });
     },
   });
 
-  const debugAddAddessMutation = async () => {
-    console.log("debugAddAddessMutation");
-    // runAddAddessMutation()
-    let ret = await client
-      .mutate({
-        mutation: aQM.CREATE_ADDRESS,
-        variables: { request: AddressRequestForCreate },
-        context: {
-          headers: {
-            isPrivate: false,
-          },
-        },
-      })
-      .then((result) => result)
-      .catch((err) => {
-        console.log("mutation error " + err);
-      });
-    console.log(`debugAddAddessMutation ${JSON.stringify(ret)}`);
-    if (typeof ret !== "undefined") {
-      console.log(`debugAddAddessMutation ${JSON.stringify(ret)}`);
-      createAddressOnComplete(ret);
-    }
-  };
-
-  const createAddressOnComplete = async (result) => {
-    console.log(`createAddressOnComplete ${JSON.stringify(result.data)}`);
-    if (typeof result.data !== "undefined") {
-      userProfileVar({
-        ...userProfileVar(),
-        addressId: result.data.createAddress.addressId,
-        addressLine1: gqlMappers.mapGQLAddressToDelivery(
-          result.data.createAddress
-        ),
-        addressLine2: gqlMappers.mapGQLAddressToLine2(
-          result.data.createAddress
-        ),
-      });
-      // added callback address, we need this one in appollo cache
-      localCartVar({
-        ...localCartVar(),
-        deliverAddress: result.data.createAddress.addressId,
-        callBackAddress: gqlMappers.mapGQLAddressResponseToCache(
-          result.data.createAddress
-        ),
-      });
-    }
-  };
-
+  const inputs = [
+    {
+      placeholder: "Address Name (ex. home)*",
+      onChangeText: (text) => setName(text),
+      showError: false,
+      errorMessage: null,
+      keyboardType: "default",
+      type: "normal",
+      value: name,
+    },
+    {
+      placeholder: "Street Name*",
+      onChangeText: (text) => setStreetName(text),
+      showError: false,
+      errorMessage: null,
+      keyboardType: "default",
+      type: "normal",
+      value: streetName,
+    },
+    {
+      placeholder: "Street Number*",
+      onChangeText: (text) => setStreetNum(text),
+      showError: false,
+      errorMessage: null,
+      keyboardType: "default",
+      type: "short",
+      value: streetNum,
+    },
+    {
+      placeholder: "Flat, Floor, Door",
+      onChangeText: (text) => setDoor(text),
+      showError: false,
+      errorMessage: null,
+      keyboardType: "default",
+      type: "short",
+      value: door,
+    },
+    {
+      placeholder: "City*",
+      onChangeText: (text) => setCity(text),
+      showError: false,
+      errorMessage: null,
+      keyboardType: "default",
+      type: "short",
+      value: city,
+    },
+    {
+      placeholder: "State*",
+      onChangeText: (text) => setMstate(text),
+      showError: false,
+      errorMessage: null,
+      keyboardType: "selector",
+      type: "short",
+      value: mstate,
+    },
+    {
+      placeholder: "Pincode*",
+      onChangeText: (text) => setPincode(text),
+      showError: false,
+      errorMessage: null,
+      keyboardType: "decimal-pad",
+      type: "short",
+      value: pincode,
+    },
+    {
+      placeholder: "Country*",
+      onChangeText: (text) => setCountry(text),
+      showError: false,
+      errorMessage: null,
+      keyboardType: "default",
+      type: "short",
+      value: country,
+    },
+    {
+      placeholder: "Land Mark",
+      onChangeText: (text) => setLandMark(text),
+      showError: false,
+      errorMessage: null,
+      keyboardType: "default",
+      type: "normal",
+      value: landMark,
+    },
+  ];
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.popupHeader}>
-        <Text style={[styles.txtSave, { color: "transparent" }]}>SAVE</Text>
-        <Text style={styles.popupTitle}>Add your delivery address</Text>
-        <TouchableOpacity
-          onPress={() => {
-            // had to do this for issues getting local state for create address
-            debugAddAddessMutation();
-            //debugAddAddessMutation()
-            // will close sheet
-            toggleAddLocationSheet();
-          }}
-        >
-          <Text style={styles.txtSave}>SAVE</Text>
-        </TouchableOpacity>
-      </View>
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.background} />
+      <SafeAreaView
+        style={styles.safeArea}
+        edges={["top", "right", "left", "bottom"]}
+      >
+        <View style={styles.popupHeader}>
+          <Text style={[styles.txtSave, { color: "transparent" }]}>SAVE</Text>
+          <Text style={styles.popupTitle}>Add your delivery address</Text>
+          <TouchableOpacity
+            onPress={() => {
+              // // had to do this for issues getting local state for create address
+              // debugAddAddessMutation();
+              // //debugAddAddessMutation()
+              // // will close sheet
+              // toggleAddLocationSheet();
+            }}
+          >
+            <Text style={styles.txtSave}>SAVE</Text>
+          </TouchableOpacity>
+        </View>
 
-      <KeyboardAwareScrollView enableOnAndroid>
-        <TextInput
-          onChangeText={(text) => setPinCode(text)}
-          placeholder={"Pin Code"}
-          style={styles.textInput}
-          returnKeyType={"next"}
-          onSubmitEditing={() => stateInput.getInnerRef().focus()}
-        />
-        <TextInput
-          onChangeText={(text) => setStateV(text)}
-          placeholder={"State (Province)"}
-          style={styles.textInput}
-          ref={(r) => (stateInput = r)}
-          returnKeyType={"next"}
-          onSubmitEditing={() => cityInput.getInnerRef().focus()}
-        />
-        <TextInput
-          onChangeText={(text) => setCityV(text)}
-          placeholder={"Town or city"}
-          style={styles.textInput}
-          ref={(r) => (cityInput = r)}
-          returnKeyType={"next"}
-          onSubmitEditing={() => villageInput.getInnerRef().focus()}
-        />
-        <TextInput
-          onChangeText={(text) => setAreaV(text)}
-          placeholder={"Village or area"}
-          style={styles.textInput}
-          ref={(r) => (villageInput = r)}
-          returnKeyType={"next"}
-          onSubmitEditing={() => houseNumberInput.getInnerRef().focus()}
-        />
-        <TextInput
-          onChangeText={(text) => setNumHV(text)}
-          placeholder={"House number"}
-          style={styles.textInput}
-          ref={(r) => (houseNumberInput = r)}
-          returnKeyType={"next"}
-          onSubmitEditing={() => flatNumberInput.getInnerRef().focus()}
-        />
-        <TextInput
-          onChangeText={(text) => setNumFV(text)}
-          placeholder={"Flat number"}
-          style={styles.textInput}
-          ref={(r) => (flatNumberInput = r)}
-          returnKeyType={"next"}
-          onSubmitEditing={() => landmarkInput.getInnerRef().focus()}
-        />
-        <TextInput
-          onChangeText={(text) => setMarkV(text)}
-          placeholder={"Landmark"}
-          style={styles.textInput}
-          ref={(r) => (landmarkInput = r)}
-          returnKeyType={"done"}
-        />
-      </KeyboardAwareScrollView>
+        <View style={styles.bodyContainer}>
+          <KeyboardAwareScrollView>
+            <View
+              style={{
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+              }}
+            >
+              {inputs.map((item, index) => {
+                return (
+                  <View
+                    key={index}
+                    style={{
+                      width: item.type == "short" ? "48%" : "100%",
+                      marginTop: vs(18),
+                    }}
+                  >
+                    {item.keyboardType === "selector" ? (
+                      <Selector
+                        style={{ marginBottom: vs(10) }}
+                        placeholder={"Sate"}
+                        value={mstate}
+                        data={["AAA", "BBB", "CCC"]}
+                        onValueChange={(text) => setMstate(text)}
+                      />
+                    ) : (
+                      <MaterialTextInput
+                        style={{ marginTop: vs(18) }}
+                        {...item}
+                      />
+                    )}
+                  </View>
+                );
+              })}
+            </View>
+          </KeyboardAwareScrollView>
+          <View style={{ marginTop: 20 }}>
+            <Switch
+              onSwitch={(res) => {
+                setAsDefault(res);
+              }}
+              active={asDefault}
+              label="Set as default address"
+            />
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
+
+export default AddLocationSheetContent;

bab0ecb778a47eac9d0a068a1b083ba7a2177e0a
Jianlong Nie <1317272927@qq.com>
2021-08-03 07:37:51 +0800

    variant component

--- App/Containers/Explore/Components/Variants/index.js
+++ App/Containers/Explore/Components/Variants/index.js
@@ -29,30 +29,10 @@ import Variant from "./Variant";
  *  props.product.prodVariants
  *  array of variants for this product
  */
-const ProductVariants = (props) => {
-  // will probably need to pass the product down to variants so it can be updated on selection
-  const [product, setProductFromProps] = useState({});
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    console.log("variants constructor");
-    setProductFromProps(props.product);
-    // console.log(JSON.stringify(product));
-    setIsReady(true);
-  }, [props, product]);
-
+const ProductVariants = ({ listingVariants }) => {
   // to do reduce number props if possible
-  if (isReady) {
-    return (
-      <View style={styles.pageContainer}>
-        {/* {product.prodVariants.map((variant, index) => {
-          return <Variant variant={variant} product={product} key={index} />;
-        })} */}
-      </View>
-    );
-  } else {
-    return <View style={styles.pageContainer} />;
-  }
+
+  return <View style={styles.pageContainer} />;
 };
 
 const styles = ScaledSheet.create({

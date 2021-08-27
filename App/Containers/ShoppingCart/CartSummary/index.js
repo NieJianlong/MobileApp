acd31fa7271f5013ecddee406119dc64163b6290
Jianlong Nie <1317272927@qq.com>
2021-08-27 15:07:12 +0800

    updates

--- App/Containers/ShoppingCart/CartSummary/index.js
+++ App/Containers/ShoppingCart/CartSummary/index.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { View, Text } from "react-native";
 import { s, vs } from "react-native-size-matters";
 s;
 import AppConfig from "../../../Config/AppConfig";
+import useRealm from "../../../hooks/useRealm";
 import colors from "../../../Themes/Colors";
 import styles from "../styles";
+import PubSub from "pubsub-js";
 
-function index(props) {
+function CartSummary(props) {
+  const { realm } = useRealm();
+  const [mydatas, setMydatas] = useState(realm.objects("ShoppingCart"));
+  useEffect(() => {
+    let refresh = PubSub.subscribe("refresh-shoppingcart", () => {
+      setMydatas(realm.objects("ShoppingCart"));
+    });
+    return () => {
+      PubSub.unsubscribe(refresh);
+    };
+  }, [realm]);
+  const quantity = useMemo(() => {
+    let currentQuantity = 0;
+    for (let index = 0; index < mydatas.length; index++) {
+      const element = mydatas[index];
+      currentQuantity = currentQuantity + element.cartInfo[0].quantity;
+    }
+    return currentQuantity;
+  }, [mydatas]);
   return (
     <View style={{ padding: AppConfig.paddingHorizontal }}>
       <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
@@ -33,4 +53,4 @@ function index(props) {
   );
 }
 
-export default index;
+export default CartSummary;

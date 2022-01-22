bf7d3b679b03dc7e37696b040208e0af76a670f0
Jianlong Nie <1317272927@qq.com>
2022-01-22 11:33:16 +0800

    add ts and code gen

---
+++ index.tsx
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry, LogBox } from "react-native"
+import App from "./App/Containers/App"
+import { name as appName } from "./app.json"
+import "react-native-gesture-handler"
+LogBox.ignoreAllLogs()
+AppRegistry.registerComponent(appName, () => App)

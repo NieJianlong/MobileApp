4183d9a886239d8fbcf7652ad221f3f501f29378
Jianlong Nie <1317272927@qq.com>
2022-01-26 20:28:12 +0800

    no message

--- App/Containers/ReportGroup/index.js
+++ App/Containers/ReportGroup/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
 import {
   View,
   ScrollView,
@@ -7,15 +7,15 @@ import {
   SafeAreaView,
   StatusBar,
   TextInput as RNTextInput,
-} from 'react-native';
-import AppConfig from '../../Config/AppConfig';
-import { vs, s, ScaledSheet } from 'react-native-size-matters';
-import fonts from '../../Themes/Fonts';
-import colors from '../../Themes/Colors';
-import { AppBar, RightButton } from '../../Components';
-import NavigationService from '../../Navigation/NavigationService';
+} from "react-native";
+import AppConfig from "../../Config/AppConfig";
+import { vs, s, ScaledSheet } from "react-native-size-matters";
+import fonts from "../../Themes/Fonts";
+import colors from "../../Themes/Colors";
+import { AppBar, RightButton } from "../../Components";
+import NavigationService from "../../Navigation/NavigationService";
 
-import { ApplicationStyles } from '../../Themes';
+import { ApplicationStyles } from "../../Themes";
 
 function ReportGroup(props) {
   const [showPrefer, setShowPrefer] = useState(false);
@@ -24,7 +24,7 @@ function ReportGroup(props) {
       style={{
         flex: 1,
         backgroundColor: colors.background,
-        position: 'absolute',
+        position: "absolute",
         top: 0,
         left: 0,
         right: 0,
@@ -34,14 +34,14 @@ function ReportGroup(props) {
       <StatusBar barStyle="dark-content" backgroundColor={colors.background} />
       <SafeAreaView
         style={styles.safeArea}
-        edges={['top', 'right', 'left', 'bottom']}
+        edges={["top", "right", "left", "bottom"]}
       >
         <AppBar
           rightButton={() => (
             <RightButton
               title="SUBMIT"
               onPress={() => {
-                NavigationService.navigate('RefundScreen', { cancel: true });
+                NavigationService.navigate("RefundScreen", { cancel: true });
               }}
             />
           )}
@@ -52,7 +52,7 @@ function ReportGroup(props) {
               fontSize: s(22),
               fontFamily: fonts.primary,
               color: colors.black,
-              fontWeight: '600',
+              fontWeight: "600",
             }}
           >
             Report
@@ -61,11 +61,11 @@ function ReportGroup(props) {
         <ScrollView style={{ paddingHorizontal: AppConfig.paddingHorizontal }}>
           <Selector
             style={{ marginTop: vs(15), marginBottom: vs(10) }}
-            placeholder={'Problem reason goes here'}
+            placeholder={"Problem reason goes here"}
             data={[
-              'The product is damaged',
-              'The product is damaged',
-              'The product is damaged',
+              "The product is damaged",
+              "The product is damaged",
+              "The product is damaged",
             ]}
             onValueChange={(item) => {
               setShowPrefer(true);
@@ -81,9 +81,9 @@ function ReportGroup(props) {
               borderRadius: s(20),
               borderWidth: 1,
               borderColor: colors.grey20,
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              flexDirection: 'row',
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexDirection: "row",
               padding: s(14),
               paddingVertical: s(20),
             }}
@@ -98,8 +98,8 @@ export default ReportGroup;
 const styles = ScaledSheet.create({
   title: {
     fontFamily: fonts.primary,
-    fontSize: '16@s',
+    fontSize: "16@s",
     color: colors.black,
-    fontWeight: '600',
+    fontWeight: "600",
   },
 });

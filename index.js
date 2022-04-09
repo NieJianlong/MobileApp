882c26ac1cf4e0905029f58bdf2973032712d4f4
Jianlong Nie <1317272927@qq.com>
2022-04-09 21:02:27 +0800

    updates

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /**
  * @format
  */
+import "expo-asset";
 import { AppRegistry, LogBox } from "react-native";
 import App from "./App/Containers/App";
-import { name as appName } from "./app.json";
 import "react-native-gesture-handler";
+
 LogBox.ignoreAllLogs();
 AppRegistry.registerComponent("apollo", () => App);

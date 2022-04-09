2da7336748b182b7663c6992a74c2d46d500a42e
Jianlong Nie <1317272927@qq.com>
2022-04-09 16:36:02 +0800

    fix error

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /**
  * @format
  */
-
-import { AppRegistry, LogBox } from "react-native"
-import App from "./App/Containers/App"
-import { name as appName } from "./app.json"
-import "react-native-gesture-handler"
-LogBox.ignoreAllLogs()
-AppRegistry.registerComponent(appName, () => App)
+import { AppRegistry, LogBox } from "react-native";
+import App from "./App/Containers/App";
+import { name as appName } from "./app.json";
+import "react-native-gesture-handler";
+LogBox.ignoreAllLogs();
+AppRegistry.registerComponent("apollo", () => App);

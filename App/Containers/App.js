e93c49e173d683af22caa6230ce3d7164e799f18
Jianlong Nie <1317272927@qq.com>
2022-04-14 09:50:31 +0800

    sd

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import RootContainer from "./Root";
 /**
  * we are using apollo for gql and state
@@ -6,14 +6,37 @@ import RootContainer from "./Root";
  */
 import { ApolloProvider } from "@apollo/client";
 /** pubClient is gql client for public api and is also the global cache */
+import * as SplashScreen from "expo-splash-screen";
+import Realm from "realm";
+import { RealmConnector } from "../db/connector";
 import { client } from "../Apollo/apolloClient";
-import { View } from "react-native";
+const realm = await Realm.open(RealmConnector);
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <RootContainer />
-    {/* <View /> */}
-  </ApolloProvider>
-);
+/// ReactNativeFlipperDatabases - START
+
+if (__DEV__) {
+  // Import connectDatabases function and required DBDrivers
+  const {
+    connectDatabases,
+    RealmDB,
+  } = require("react-native-flipper-databases");
+
+  connectDatabases([
+    new RealmDB("mobile.realm", realm), // Pass in a realm name and an open realm reference
+  ]);
+}
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  return (
+    <ApolloProvider client={client}>
+      <RootContainer />
+      {/* <View /> */}
+    </ApolloProvider>
+  );
+};
 
 export default App;

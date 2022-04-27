0ea47288b6d98d8fdb8dfa77f19aff4f11ad400b
Jianlong Nie <1317272927@qq.com>
2022-04-27 16:54:02 +0800

    animation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import { AppRegistry, LogBox } from "react-native";
 import App from "./App/Containers/App";
 import "react-native-gesture-handler";
-// import * as SplashScreen from "expo-splash-screen";
-// SplashScreen.hideAsync();
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.hideAsync();
 LogBox.ignoreAllLogs();
 AppRegistry.registerComponent("apollo", () => App);

b31e7da4dc3226cc27eaebcfa1eafef4769e0182
Jianlong Nie <1317272927@qq.com>
2022-07-27 15:48:07 +0800

    stage for maria

--- App/Config/AppConfig.ts
+++ App/Config/AppConfig.ts
@@ -3,8 +3,9 @@ import { s } from "react-native-size-matters";
 const AppConfig = {
   fontSize: s(15),
   paddingHorizontal: s(15),
+  baseUrl: "https://stage-api.salamislicing.in/",
   // baseUrl: "http://ec2-18-117-210-203.us-east-2.compute.amazonaws.com",
-  baseUrl: "https://dev-api.salamislicing.in/",
+  // baseUrl: "https://dev-api.salamislicing.in/",
   guestId: "9fcbb7cb-5354-489d-b358-d4e2bf386ff3",
 };
 

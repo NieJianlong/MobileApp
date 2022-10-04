10c6b7cd56493e70096c38a898ea0254176b6cab
Jianlong Nie <1317272927@qq.com>
2022-10-04 14:27:28 +0800

    update new version

--- App/Config/AppConfig.ts
+++ App/Config/AppConfig.ts
@@ -1,12 +1,22 @@
 import { s } from "react-native-size-matters";
 import * as Updates from "expo-updates";
+import DeviceInfo from "react-native-device-info";
+import { Platform } from "react-native";
+let url = Updates.releaseChannel.startsWith("prod")
+  ? "https://api.salamislicing.in/"
+  : "https://stage-api.salamislicing.in/";
+if (Platform.OS === "ios") {
+  if (__DEV__) {
+    url = "https://stage-api.salamislicing.in/";
+  } else {
+    url = "https://api.salamislicing.in/";
+  }
+}
 
 const AppConfig = {
   fontSize: s(15),
   paddingHorizontal: s(15),
-  baseUrl: Updates.releaseChannel.startsWith("prod")
-    ? "https://api.salamislicing.in/"
-    : "https://stage-api.salamislicing.in/",
+  baseUrl: url,
   // baseUrl: "http://ec2-18-117-210-203.us-east-2.compute.amazonaws.com",
   // baseUrl: "https://dev-api.salamislicing.in/",
   guestId: "9fcbb7cb-5354-489d-b358-d4e2bf386ff3",
